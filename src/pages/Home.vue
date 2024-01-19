8fd1c144e91579145b53a7ac293eaba30346a8d2
Mattia2700 <mattia2700franzin+dev@gmail.com>
2024-01-19 16:02:55 +0100

    chore: bottom working

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,13 +14,13 @@ function redirectToAnswer() {
 </script>
 
 <template>
-  <div class="mt-[1.5em] justify-center">
+  <div class="mt-[1.5em] flex justify-center">
     <p class="text-left font-montserrat font-normal leading-5 text-[16px] pl-2 border-l-4 border-l-[#e600fa] rounded-l-sm">
       Scegli se questo dispositivo sarà usato per
       <span class="font-semibold">rispondere</span> o per
       <span class="font-semibold">fare le domande</span>
     </p>
-    <div class="mt-[50%] mx-auto">
+    <div class="fixed bottom-14 flex flex-col items-center justify-center w-full px-4 md:px-[30%]">
       <button @click="redirectToAnswer"
         class="text-black text-base font-semibold font-montserrat bg-white uppercase w-full py-4 shadow-sm rounded-2xl">
         Rispondere
