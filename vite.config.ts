f875ac28f592e230aff7bfcc0da1d77842911e43
Mattia2700 <mattia2700franzin+dev@gmail.com>
2024-01-20 14:24:29 +0100

    fix: basePath pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cicapiamo/",
   plugins: [
     vue(),
     VitePWA({
-      includeAssets: ['/vite.svg', '/pwa-192x192.png', '/pwa-512x512.png'],
+      includeAssets: ['/cicapiamo/vite.svg', '/cicapiamo/pwa-192x192.png', '/cicapiamo/pwa-512x512.png'],
       manifest: {
         name: "Ci Capiamo?",
         short_name: "CiCapiamo",
@@ -16,30 +17,30 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/cicapiamo/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/cicapiamo/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/cicapiamo/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/cicapiamo/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
           }
         ]
       },
-      injectRegister: 'script',
+      injectRegister: null,
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
@@ -52,8 +53,7 @@ export default defineConfig({
     },
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
-  base: process.env.NODE_ENV === "production" ? "/cicapiamo/" : "/",
   define: {
     __APP_ID__: JSON.stringify("cicapiamo"),
-  }
+  },
 });
