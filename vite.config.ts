ec4f1d2a89098f85f4a391f182ade5626df41fce
Mattia2700 <mattia2700franzin+dev@gmail.com>
2024-01-20 02:06:52 +0100

    chore: adapt base url

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     },
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
+  base: process.env.NODE_ENV === "production" ? "/cicapiamo/" : "/",
   define: {
     __APP_ID__: JSON.stringify("cicapiamo"),
   }
