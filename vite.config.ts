dd97ef2349efcab0f0c2a47d7e7dfa72f4fb886b
Mattia2700 <mattia2700franzin+dev@gmail.com>
2024-01-20 13:24:26 +0100

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,51 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['public/vite.svg'],
+      manifest: {
+        name: "Ci Capiamo?",
+        short_name: "CiCapiamo",
+        description: "Ci Capiamo? - A game to play with your friends",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: 'public/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'public/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'public/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'public/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      injectRegister: 'script',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
