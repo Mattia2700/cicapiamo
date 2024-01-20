719fd3753b0bf6ed4e9f897ee2269a781de7de31
Mattia2700 <mattia2700franzin+dev@gmail.com>
2024-01-20 13:49:31 +0100

    fix: production pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      includeAssets: ['public/vite.svg'],
+      includeAssets: ['/vite.svg', '/pwa-192x192.png', '/pwa-512x512.png'],
       manifest: {
         name: "Ci Capiamo?",
         short_name: "CiCapiamo",
@@ -16,23 +16,23 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: 'public/pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'public/pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'public/pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'public/pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
