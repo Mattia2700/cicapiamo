54cd4c37a128e6f5fb2e182e8e1580f3ce3084de
Mattia2700 <mattia2700franzin+dev@gmail.com>
2024-01-24 10:23:29 +0100

    feat: disable pwa in app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import StepRow from "@/components/StepRow.vue";
 import { RouterView, useRouter } from "vue-router";
-import { computed, ref } from "vue";
-import Pwa from "@/components/Pwa.vue";
+import { computed } from "vue";
+//import Pwa from "@/components/Pwa.vue";
 
-const isPWA = computed(() => {
-  return window.matchMedia("(display-mode: standalone)").matches;
-});
+//const isPWA = computed(() => {
+//  return window.matchMedia("(display-mode: standalone)").matches;
+//});
 const currentRoute = computed(() => {
   if (router.currentRoute.value.name === "home") {
     return 1;
@@ -16,23 +16,21 @@ const currentRoute = computed(() => {
     return 3;
   }
 });
-const playAnyway = ref(false);
+//const playAnyway = ref(false);
 const router = useRouter();
-
-
 
 </script>
 <template>
   <RouterView v-slot="{ Component }">
     <div class="mt-4 p-1 px-[1.5rem] text-white max-w-lg m-auto flex flex-col">
       <img class="flex" src="@/assets/it-title.svg" alt="Ci Capiamo?" />
-      <div v-if="isPWA || playAnyway">
+      <div>
         <StepRow :current_idx="currentRoute" />
         <Component :is="Component" />
       </div>
-      <div v-else>
-        <Pwa @play="playAnyway = true"/>
-      </div>
+      <!--<div v-else>-->
+        <!--<Pwa @play="playAnyway = true"/>-->
+      <!--</div>-->
     </div>
   </RouterView>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,57 +1,56 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cicapiamo/",
+  // base: "/cicapiamo/",
   plugins: [
     vue(),
-    VitePWA({
-      includeAssets: ['/cicapiamo/vite.svg', '/cicapiamo/pwa-192x192.png', '/cicapiamo/pwa-512x512.png'],
-      manifest: {
-        name: "Ci Capiamo?",
-        short_name: "Ci Capiamo?",
-        description: "Ci Capiamo? - A game to play with your friends",
-        theme_color: "#000000",
-        related_applications: [
-          {
-            platform: "webapp",
-            url: "http://localhost:5173/cicapiamo/manifest.webmanifest"
-          }
-        ],
-        icons: [
-          {
-            src: '/cicapiamo/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/cicapiamo/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: '/cicapiamo/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/cicapiamo/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      },
-      injectRegister: null,
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    })
+    // VitePWA({
+    //   includeAssets: ['/cicapiamo/vite.svg', '/cicapiamo/pwa-192x192.png', '/cicapiamo/pwa-512x512.png'],
+    //   manifest: {
+    //     name: "Ci Capiamo?",
+    //     short_name: "Ci Capiamo?",
+    //     description: "Ci Capiamo? - A game to play with your friends",
+    //     theme_color: "#000000",
+    //     related_applications: [
+    //       {
+    //         platform: "webapp",
+    //         url: "http://localhost:5173/cicapiamo/manifest.webmanifest"
+    //       }
+    //     ],
+    //     icons: [
+    //       {
+    //         src: '/cicapiamo/pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: '/cicapiamo/pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: '/cicapiamo/pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any'
+    //       },
+    //       {
+    //         src: '/cicapiamo/pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'maskable'
+    //       }
+    //     ]
+    //   },
+    //   injectRegister: null,
+    //   registerType: 'autoUpdate',
+    //   devOptions: {
+    //     enabled: true
+    //   }
+    // })
   ],
   resolve: {
     alias: {
