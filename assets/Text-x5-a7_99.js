106c0cd96a61437394a38991bc0d3cc46cd2e874
Mattia2700 <mattia2700franzin+dev@gmail.com>
2024-01-24 01:41:02 +0100

    Updates

---
+++ assets/Text-x5-a7_99.js
@@ -0,0 +1,1 @@
+import{c as s,h as c,o as a}from"./index-Gf4OWD9e.js";const l=(t,e)=>{const o=t.__vccOpts||t;for(const[r,n]of e)o[r]=n;return o},_={},d={class:"mt-[1.5em] text-left font-montserrat font-normal leading-5 text-[16px] pl-2 border-l-4 border-l-[#e600fa] rounded-l-sm"};function f(t,e){return a(),s("div",d,[c(t.$slots,"default")])}const p=l(_,[["render",f]]);export{p as T,l as _};
