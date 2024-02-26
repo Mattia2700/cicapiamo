b7a0dda49bcc507e7b6dbdc1471eab129de2ad0c
Mattia2700 <mattia2700franzin+dev@gmail.com>
2024-02-26 22:53:51 +0100

    feat: add custom button

--- src/components/AnswerButton.vue
+++ src/components/AnswerButton.vue
@@ -1,46 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  disabled: Boolean,
+});
+</script>
 
 <template>
-  <div class="flex flex-col relative">
-    <span
-      id="border"
-      class="h-40 w-40 absolute top-2 rounded-full bg-[#b300c2]"
-    ></span>
-    <span
-      id="bottom"
-      class="h-40 w-[166px] absolute top-5 rounded-full bg-[#690083]"
-    ></span>
-    <span id="bottom" class="top-20 h-7 w-[166px] absolute bg-[#690083]"></span>
-    <span
-      id="border"
-      class="h-40 w-40 z-0 bg-white hover:mt-2 rounded-full flex items-center justify-center bg-gradient-to-b from-[#e600fa] to-[#b300c2]"
-    >
-      <p class="font-montserrat font-extrabold leading-5 uppercase text-[20px]">
-        Rispondi
-      </p>
-    </span>
-  </div>
+  <img
+    src="@/assets/locked.png"
+    class="max-h-[230px] pt-[12px] pr-[6px]"
+    alt="disabled"
+    v-if="disabled"
+  />
+  <img
+    class="max-h-[230px] active:pt-[20px] active:pr-[3px]"
+    alt="active"
+    v-else
+  />
 </template>
 
 <style scoped>
-p {
-  color: transparent;
-  text-shadow:
-    0 -1px 1px #690083,
-    0 0 0 black,
-    0 -1px 1px black;
+img[alt="active"] {
+  content: url("@/assets/active.png");
 }
 
-#border {
-  box-shadow:
-    1px 0 6px #b800c9,
-    1px 0 6px #b800c9,
-    0 1px 6px #b800c9,
-    0 1px 6px #b800c9;
-  transform: rotateX(30deg);
-}
-
-#bottom {
-  transform: rotateX(30deg) translateX(-3px);
+img[alt="active"]:active {
+  content: url("@/assets/pressed.png");
 }
 </style>
